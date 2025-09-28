290820dc3b99bb338921bb55dddc0a3b39c60e41
rahmatpn <rahmatpn555@gmail.com>
2025-09-28 14:36:01 +0700

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output  : "export",
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
 };
 
 export default nextConfig;
